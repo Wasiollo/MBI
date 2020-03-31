9360f23aa58f874e1475a18fc9e613a9644b1847
Wasiollo <wasiak.mk@gmail.com>
2020-03-31 12:21:40 +0200

    fix for base tag

--- src/services/router-helper/router-helper.service.js
+++ src/services/router-helper/router-helper.service.js
@@ -3,7 +3,10 @@ import appRoutes from './app-routes';
 export default function routerHelper($stateProvider, $urlRouterProvider, $locationProvider) {
     'ngInject';
 
-    $locationProvider.html5Mode(true); // setting html5 mode to remove !# from url
+    $locationProvider.html5Mode({
+        enabled: true,
+        requireBase: false
+    }); // setting html5 mode to remove !# from url
     $urlRouterProvider.otherwise('/'); // setting default route
 
     appRoutes.forEach((route) => {
