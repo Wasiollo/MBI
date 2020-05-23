ebce4424719dffc19ce735fcfc9cecb2e7b90133
Mateusz Plesinski <mateusz.plesinski@gmail.com>
2020-05-23 19:31:31 +0200

    Added suffix tree impl

--- src/services/chart/chart.service.js
+++ src/services/chart/chart.service.js
@@ -1,6 +1,153 @@
 import * as math from 'mathjs';
 import * as _ from 'lodash';
 
+class SuffixTree {
+    constructor(str) {
+        this.root = {
+            id: 0,
+            children: {},
+            start:0
+        }
+        this.activeNode = this.root;
+        this.curr_pos = -1; //left
+        this.look_ahead = -1;   //right
+        this.last_string_idx = -1;  //idx
+        this.strings_list = {};     
+        this.last_node_id = 0;  //lastID
+        this.skip = 0;
+        this.text = [];
+        if(str) {
+            this.add(str);
+        }
+    }
+
+    add (str) {
+        if(!(typeof str === 'string') || str.length == 0){
+            return;
+        }
+        this.last_string_idx += 1;
+        this.strings_list[this.last_string_idx] = str;
+
+        for(let i = 0; i < str.length; ++i) {
+            this.addSingleChar(str[i]);
+        }
+        this.addSingleChar('$' + this.last_node_id);
+    }
+
+    addSingleChar(char) {
+        this.look_ahead += 1;
+        this.text.push(char);
+
+        let prev_internal_node;
+
+        while(this.curr_pos < this.look_ahead) {
+            let skipped = this.curr_pos + this.skip;
+
+            if(skipped < this.look_ahead) {
+                let edge_char = this.text[skipped + 1];
+                let edge = this.activeNode.children[edge_char];
+                if(edge) {
+                    let edge_length = this.getNodeLength(edge);
+                    let extension = this.look_ahead - (this.curr_pos + this.skip);
+
+                    while(edge_length < extension) {
+                        this.activeNode = edge;
+                        this.skip += edge_length;
+                        extension = this.look_ahead - (this.curr_pos + this.skip);
+
+                        if(!extension) {
+                            break;
+                        }
+
+                        edge_char = this.text[this.curr_pos + this.skip + 1];
+                        edge = this.activeNode.children[edge_char];
+
+                        if(!edge) {
+                            break;
+                        }
+
+                        edge_length = this.getNodeLength(edge);
+                    }
+                }
+            }
+
+            const node = this.activeNode;
+
+            skipped = this.curr_pos + this.skip;
+            let extension = this.look_ahead - skipped - 1;
+
+            if(extension <= 0) {
+                if(node.children[char]) {
+                    return true;
+                } else {
+                    node.children[char] = this.createNode(this.look_ahead, node);
+                    this.curr_pos += 1;
+                    this.activeNode = this.root;
+                    this.skip = 0;
+                }
+            } else {
+                const edge_char = this.text[skipped + 1];
+                const edge = node.children[edge_char];
+                const extension = this.look_ahead - skipped - 1;
+                const match_char = this.text[edge.start + extension];
+
+                if(char === match_char) {
+                    return;
+                } else {
+                    const end_child = this.createNode(this.look_ahead, edge);
+                    const split_child = this.createNode(edge.start + extension, edge);
+
+                    if(edge.end !== undefined) {
+                        const split_child_length = this.getNodeLength(edge) - extension;
+                        split_child.end = split_child.start + split_child_length - 1;
+
+                        for(let child_char in edge.children) {
+                            split_child.children[child_char] = edge.children[child_char];
+                            delete edge.children[child_char];
+                        }
+                    }
+                    edge.end = edge.start + extension - 1;
+                    edge.children[char] = end_child;
+                    end.children[match_char] = split_child;
+
+                    if(prev_internal_node) {
+                        prev_internal_node.link = edge;
+                    }
+                    prev_internal_node = edge;
+
+                    if(!node.link || node.link.parent === this.root) {
+                        this.activeNode = this.root;
+                    } else {
+                        this.activeNode = node.link;
+                    }
+
+                    if(this.activeNode === this.root) {
+                        this.skip = 0;
+                    } else {
+                        this.skip -= 1;
+                    }
+                    this.curr_pos += 1;
+                }
+            }
+        }
+    }
+
+    getNodeLength(node) {
+        const end = node.end === undefined ? this.look_ahead : node.end;
+        return end + 1 - node.start;
+    }
+
+    createNode(start, parent) {
+        this.last_node_id += 1;
+        return {
+            start: start,
+            children: {},
+            id: this.last_node_id,
+            parent
+        }
+    }
+}
+
 export default class ChartService {
     constructor($rootScope) {
         'ngInject';
@@ -18,7 +165,7 @@ export default class ChartService {
 
     ajd_to_path_matrix(adj) {
         const n = math.size(adj)[0];
-        let path_mat = adj;
+        let path_mat = math.matrix(adj);
         for (let i = 1; i < n; ++i) {
             let path_mat_i = path_mat;
             for (let j = 0; j < i; ++j) {
@@ -28,16 +175,46 @@ export default class ChartService {
         }
 
         path_mat.map(function (value, index, matrix) {
-            matrix[index] = value > 0 ? 1 : 0;
+            matrix.set(index, value > 0 ? 1 : 0);
         });
         return path_mat;
 
     }
 
+    transitive_reduction(path_mat) {
+        const s = math.size(path_mat)[0];
+        for(let n = 1; n < s; ++n) {
+            for(let i = 1; i < s; ++i) {
+                for(let j = 0; j < s; ++j) {
+                    if(path_mat.get([i,j])) {
+                        for(let k = 0; k < i; ++k) {
+                            if(path_mat.get([i,k])) {
+                                path_mat.set([j,k], 0);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
     olc_assembly(contigs, l, k) {
         let adj_matrix = this.overlap_naive(contigs, l, k);
+        console.log(this.greedy_hpath(contigs, adj_matrix));
 
         let path_mat = this.ajd_to_path_matrix(adj_matrix);
+
+        this.transitive_reduction(path_mat);
+
+        adj_matrix.map(function (value, index, matrix) {
+            matrix.set(index, (path_mat.get(index) ? value : 0));
+        });
+
+        let tree = new SuffixTree('');
+        for(let c of contigs) {
+            tree.add(c);
+        }
+
         return this.greedy_hpath(contigs, adj_matrix);
     }
 
@@ -98,7 +275,7 @@ export default class ChartService {
     }
 
     testOlcAssembly() {
-        let seq = this.olc_assembly(['XTTG', 'TTGG', 'TGGT', 'TGXG', 'GGTT', 'TTGX', 'GTTG', 'XGXT', 'GXGX'], 2, 4);
+        let seq = this.olc_assembly(['XTTG', 'TTGG', 'TGGT', 'TGXG', 'GGTT', 'TTGX', 'GTTG', ], 3, 4);
         console.log(seq);
     }
 }
